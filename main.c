2490e2ec9072749562fd20ccfd18ca94f0b27ce1
Farhan Ahmed Nahid <74184886+farhan-nahid@users.noreply.github.com>
2022-09-26 15:29:15 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 
-void hello_world();
+// void hello_world();
 
 int main()
 {
-    hello_world();
+    int firstNumber = 0;
+    int secondNumber = 0;
+
+    int sum = 0;
+    int sub = 0;
+    int multiplication =0;
+    int division = 0;
+
+    printf("Enter first number: ");
+    scanf("%d", &firstNumber);
+    printf("\nEnter second number: ");
+    scanf("%d", &secondNumber);
+
+
+    sum = firstNumber + secondNumber;
+    sub = firstNumber - secondNumber;
+    multiplication = firstNumber * secondNumber;
+    division = firstNumber / secondNumber;
+
+    printf("Sum %d\n", sum);
+    printf("Sub %d\n", sub);
+    printf("Multiplication %d\n", multiplication);
+    printf("Division %d\n", division);
     return 0;
     
 }
+
+/*
+
 
 void hello_world()
 {
@@ -19,3 +44,5 @@ void hello_world()
     printf("my age is %d. \n", age);
     printf("my age is %f. \n", 22.2);
 }
+
+*/
